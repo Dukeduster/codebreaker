237289911cd33b698320e79a29007fd7bbf9c056
Dukeduster <lopcamilo@gmail.com>
2018-11-10 11:35:35 -0500

    heroku

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -10,26 +10,21 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-    private final String CODE="6902";
     private  final CodeBreaker codeBreaker= new CodeBreaker();
 
     @RequestMapping("/codebreaker/guess/{id}")
     public ResponseEntity<Resultado> guess(@PathVariable String id) {
         Resultado resultado = new Resultado();
-        if(codeBreaker.guesst(id).equals("XXXX")){
-            resultado.setResultado("GOOOOL");
-        }else{
-            resultado.setResultado("NOPE");
-        }
+        //System.out.println("secret: "+secret.getSecret());
+        resultado.setResultado(codeBreaker.guesst(id));
         return ResponseEntity.ok().body(resultado);
     }
 
     @PostMapping("/codebreaker/guess")
     public ResponseEntity<Resultado> guess(@RequestBody Secret secret) {
         Resultado resultado = new Resultado();
-        CodeBreaker c = new CodeBreaker();
-        System.out.println("secret: "+secret.getSecret());
-        resultado.setResultado(codeBreaker.guesst(secret.getSecret()));
+        resultado.setResultado("OK");
+        codeBreaker.setSecret(secret.getSecret());
         return ResponseEntity.ok().body(resultado);
     }
 }
